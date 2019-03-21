61aecfc18ee513751f8f2e6883788e803d68ab91
Cristian Pinto <cdpp5@hotmail.com>
2019-03-21 15:41:25 -0500

    Created Principle Variables

---
+++ js/app.js
@@ -0,0 +1,4 @@
+const country = document.querySelector('#country');
+const gender = document.querySelector('#gender');
+const quantity = document.querySelector('#quantity');
+const form = document.querySelector('#form');
