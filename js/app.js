a8052cdba849f5c85cf489a3406642db7ec7a1ad
Cristian Pinto <cdpp5@hotmail.com>
2019-03-21 15:41:57 -0500

    Added Event Listener for Form

--- js/app.js
+++ js/app.js
@@ -2,3 +2,25 @@ const country = document.querySelector('#country');
 const gender = document.querySelector('#gender');
 const quantity = document.querySelector('#quantity');
 const form = document.querySelector('#form');
+
+// Event Listener
+form.addEventListener('submit', handleForm);
+
+function handleForm (e) {
+    e.preventDefault();
+    const listName =document.querySelector('#listNames');
+    if (listName.innerHTML != '') {
+        listName.innerHTML = '';
+    }
+    let url = 'https://uinames.com/api/?'; 
+    if (country.value !== ""){
+        url = `${url}region=${country.value}&`;
+    }
+    if (gender.value !== ""){
+        url = `${url}gender=${gender.value}&`;
+    }
+    if (quantity.value !== ""){
+        url = `${url}amount=${quantity.value}&`; 
+    }
+    requestApi(url);
+}
